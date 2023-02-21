790681cdcafd0eb0213e617bc53ed866a7ffc3ef
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2023-02-21 13:20:08 -0800

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const Database = new Datastore('database.db');
+Database.loadDatabase();
+
+app.post('/', (request, response) => {
+  const data = request.body;
+  console.log('It seems that we have recieved a request!')
+  console.log(`Body request: ${data}`)
+  if (!Database.find({user: data.user})) {
+    
+  }
+});
+
+app.get('/', (request, response) => {
+  // Yes, completely pointless, doesn't really matter though.
+  
+})
